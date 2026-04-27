8354d21115cfbfa0ab01132ca2552f8ef470a282
Aleksey Osipov <99037915+Hattaeck@users.noreply.github.com>
2026-04-27 17:45:14 +0300

    Update ProductManagerTest.java

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -1,7 +1,7 @@
 package ru.netology.manager;
 
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -9,120 +9,59 @@ import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
 public class ProductManagerTest {
-    ProductRepository repo = new ProductRepository();
-    ProductManager manager = new ProductManager(repo);
+    private ProductRepository repo = new ProductRepository();
+    private ProductManager manager = new ProductManager(repo);
 
-    Product book1 = new Book(1, "Java core", 100, "Author1", 200, 2020);
-    Product smart1 = new Smartphone(2, "Iphone 13", 1000, "Apple");
+    private Product firstBook = new Book(1, "Java Beginner", 500, "Author 1", 100, 2024);
+    private Product secondBook = new Book(2, "Advanced Java", 700, "Author 2", 200, 2025);
+    private Product smartphone = new Smartphone(3, "iPhone 15", 100000, "Apple");
 
-
-    @Test
-    public void searchByText() {
-        manager.add(book1);
-        manager.add(smart1);
-
-        Product[] expected = { smart1 };
-        Product[] actual = manager.searchBy("Iphone");
-
-        Assertions.assertArrayEquals(expected, actual);
+    @BeforeEach
+    public void setup() {
+        manager.add(firstBook);
+        manager.add(secondBook);
+        manager.add(smartphone);
     }
 
     @Test
-    public void searchByManufacturerWhenNameNotMatch() {
-        manager.add(smart1);
-
-        Product[] expected = { smart1 };
-        Product[] actual = manager.searchBy("Apple");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void searchBaseProduct() {
-        Product baseProduct = new Product(5, "Просто товар", 100);
-        manager.add(baseProduct);
-
-        Product[] expected = { baseProduct };
-        Product[] actual = manager.searchBy("Просто");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void searchByManufacture() {
-
-        Product galaxy = new Smartphone(3, "Galaxy", 5000, "Samsung");
-        manager.add(galaxy);
-
-        Product[] expected = { galaxy };
-        Product[] actual = manager.searchBy("Samsung");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void noResultSearchPhone() {
-        manager.add(smart1);
-        manager.add(book1);
-
-        Product[] expected = {};
-        Product[] actual = manager.searchBy("ТоварНеНайден");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void searchWhenMultipleProductsMatch() {
-        Book book2 = new Book(3, "Java beginner", 200, "Author2", 200, 2026);
-        manager.add(book1);
-        manager.add(book2);
-
-        Product[] expected = { book1, book2 };
+    public void shouldSearchWhenMultipleProductsFound() {
+        Product[] expected = { firstBook, secondBook };
         Product[] actual = manager.searchBy("Java");
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void noResultSearch () {
-        manager.add(smart1);
-        manager.add(book1);
+    public void shouldSearchWhenOneProductFound() {
+        Product[] expected = { smartphone };
+        Product[] actual = manager.searchBy("iPhone");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchWhenZeroProductsFound() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Microsoft");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchByBookAuthor() {
+        Product[] expected = { firstBook };
+        Product[] actual = manager.searchBy("Author 1");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchInEmptyManager() {
+        ProductRepository emptyRepo = new ProductRepository();
+        ProductManager emptyManager = new ProductManager(emptyRepo);
 
         Product[] expected = {};
-
-        Product[] actual = manager.searchBy("Puma");
-        Assertions.assertArrayEquals(expected, actual);
-
-    }
-
-    @Test
-    public void fewResultSearch () {
-        manager.add(book1);
-        manager.add(book1);
-
-        Product[] expected = {book1, book1};
-        Product[] actual = manager.searchBy("Java");
-
-        Assertions.assertArrayEquals(expected, actual);
-
-    }
-
-    @Test
-    public void searchByAuthor() {
-        manager.add(book1);
-
-        Product[] expected = { book1 };
-        Product[] actual = manager.searchBy("Author1");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void searchByAuthorWhenNameNotMatch() {
-        manager.add(book1);
-
-        Product[] expected = { book1 };
-        Product[] actual = manager.searchBy("Author1");
+        Product[] actual = emptyManager.searchBy("Java");
 
         Assertions.assertArrayEquals(expected, actual);
     }
