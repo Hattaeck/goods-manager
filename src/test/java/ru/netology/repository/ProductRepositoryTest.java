9aa4bfd4901094dfa699f23ae7c0355b5b316e0a
Aleksey <99037915+Hattaeck@users.noreply.github.com>
2026-04-27 13:45:39 +0300

    Initial commit with fixed tests

---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,45 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+  private ProductRepository repository = new ProductRepository();
+  private Book coreJava = new Book();
+
+  @Test
+  public void saveOneItem() {
+    repository.save(coreJava);
+
+    Product[] expected = new Product[]{coreJava};
+    Product[] actual = repository.findAll();
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void sRemoveById() {
+    ProductRepository repo = new ProductRepository();
+
+
+    Product book1 = new Book(1, "Java core", 100, "Author1", 200, 2020);
+    Product smart1 = new Smartphone(2, "Iphone 13", 1000, "Apple");
+
+    repo.save(book1);
+    repo.save(smart1);
+
+    repo.removeById(1);
+
+    Product[] expected = { smart1 };
+    Product[] actual = repo.findAll();
+
+    Assertions.assertArrayEquals(expected, actual);
+  }
+
+
+
+}
